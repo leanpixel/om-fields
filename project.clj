6efad17684ef2cc8c61e9525c45aa27773405e74
Rafal Dittwald <rafal.dittwald@gmail.com>
2014-11-17 13:46:19 -0500

    refactor + multi methods

--- project.clj
+++ project.clj
@@ -1,9 +1,20 @@
-(defproject org.clojars.leanpixel/om-fields "0.1.0-SNAPSHOT"
+(defproject org.clojars.leanpixel/om-fields "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.clojure/clojurescript "0.0-2280"]
+
+                 [com.andrewmcveigh/cljs-time "0.2.4"]
                  [om "0.7.1"]])
+
+  :plugins [[jamesnvc/lein-lesscss "1.3.4"]]
+
+  :lesscss-paths ["resources/less"]
+  :lesscss-output-path "resources/public/css/om-fields"
+
+  :cljsbuild {:foreign-libs [{:file "js/sugar-1.4.1-date.dev.js"
+                              :provides ["sugar.date"]}]})
---
+++ src/om_fields/thing.cljs
@@ -0,0 +1,63 @@
+(ns om-fields.thing
+    (:require-macros [cljs.core.async.macros :refer [go]])
+    (:require [om.core :as om :include-macros]
+              [om.dom :as dom :include-macros]
+              [clojure.string :refer [join]]
+              [om-fields.text :refer [human-friendly-editable]]
+              [cljs.core.async :refer [put! chan <! alts!]]
+              [om-fields.util :refer [debounce]]))
+
+(defn thing [cursor owner {:keys [update-fn edit-key search! placeholder] :as opts}]
+  (let [update-fn (or update-fn #(om/update! cursor edit-key %))]
+    (reify
+      om/IInitState
+      (init-state [_]
+        {:thing nil
+         :input-chan (chan)
+         :select-chan (chan)
+         :results [] })
+
+      om/IWillMount
+      (will-mount [_]
+        (let [select-chan (om/get-state owner :select-chan)
+              thing-result-chan (chan)
+              search-result-chan (chan)
+              input-chan (om/get-state owner :input-chan)]
+          (search! {:id (get-in cursor edit-key)} thing-result-chan)
+          (go (loop []
+                (let [[v ch] (alts! [select-chan
+                                     thing-result-chan
+                                     search-result-chan
+                                     input-chan])]
+                  (condp = ch
+                    select-chan (do (om/set-state! owner :thing v)
+                                    (om/set-state! owner :results [])
+                                    (update-fn (:db/id v)))
+                    thing-result-chan  (om/set-state! owner :thing v)
+                    search-result-chan (om/set-state! owner :results v)
+                    input-chan (if (empty? v)
+                                 (update-fn nil)
+                                 (do (om/set-state! owner :thing {:entity/name v})
+                                     (search! {:name v} search-result-chan))))
+                  (recur))))))
+
+      om/IRenderState
+      (render-state [_ state]
+        (dom/div #js {:className "autocomplete"}
+          (let [thing (state :thing)]
+            (dom/div #js {:className "mini-record"}
+              (dom/img #js {:className "image" :src (:entity/image thing)})
+              (om/build human-friendly-editable thing
+                        {:opts {:placeholder placeholder
+                                :edit-key [:entity/name]
+                                :update-fn #(put! (state :input-chan) %)}})
+              (dom/span #js {:className "type"} (join ", " (map name (:entity/is-a thing))))))
+          (apply dom/ul #js {:className "results"}
+            (map (fn [o]
+                   (dom/li #js {:onClick (fn [e] (put! (state :select-chan) o))
+                                :style #js {:cursor "pointer"}}
+                     (dom/div #js {:className "mini-record"}
+                       (dom/img #js {:className "image" :src (:entity/image o)})
+                       (dom/span #js {:className "name"} nil (:entity/name o))
+                       (dom/span #js {:className "type"} (join ", " (map name (:entity/is-a o)))))))
+                 (state :results))))))))
