c3014feffece3e0e46de35ce1a6925d643c104a2
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-01-12 22:58:09 -0500

    v1.5.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.leanpixel/om-fields "1.5.0"
+(defproject org.clojars.leanpixel/om-fields "1.5.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
