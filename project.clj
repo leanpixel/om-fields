39cc6afb71fa3369b9aa18b79da465fd1367f26c
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-03-16 14:32:50 -0400

    v1.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.leanpixel/om-fields "1.7.0"
+(defproject org.clojars.leanpixel/om-fields "1.8.0"
   :description "Fancy input components for om"
   :url "https://github.com/leanpixel/om-fields"
   :license {:name "Eclipse Public License"
