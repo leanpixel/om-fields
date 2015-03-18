7bbac0c063ac5f2a37abd456ec4400cfebf60d0e
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-03-18 10:36:14 -0400

    change sugar dep

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.clojure/clojurescript "0.0-3058"]
                  [com.andrewmcveigh/cljs-time "0.3.2"]
                  [org.omcljs/om "0.8.8"]
-                 [cljsjs/sugar "1.4.1-0"]]
+                 [org.clojars.leanpixel/sugar "1.4.1-0"]]
 
   :plugins [[jamesnvc/lein-lesscss "1.3.4"]]
 
