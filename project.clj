1a88ee421d4e2ccbb780d628e51a1571cb2e8c0d
James N. V. Cash <james.nvc@gmail.com>
2015-06-25 13:16:49 -0400

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.leanpixel/om-fields "1.8.3"
+(defproject org.clojars.leanpixel/om-fields "1.8.4"
   :description "Fancy input components for om"
   :url "https://github.com/leanpixel/om-fields"
   :license {:name "Eclipse Public License"
